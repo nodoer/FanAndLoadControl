93a4b675671c081b7b0f775e9a8cbfbf812ae984
nodoer <14099307+nodoer@users.noreply.github.com>
2018-04-23 10:54:13 -0400

    Psudeo Code
    Need Init, ConfigMenu and StandBy classes.

--- FanAndLoadControl.ino
+++ FanAndLoadControl.ino
@@ -1,56 +1,33 @@
-/*
-Screen Size 128*64
-*/
-#include "Menu.h"
-#include "RepeatMe.h"
-#include "Serpent.h"
 #include "Init.h"
+//Run Mode Programs
+#include "ConfigMenu.h"
+#include "StandBy.h"
 
-
-Init game;
-Menu menu;
-RepeatMe repeatMe;
-Serpent serpent;
-
+Init device
+ConfigMenu menu;
+StandBy standby;
 
 void setup() {
-  game.start();
-  ctrl.begin();
-  ctrl.setFrameRate(30);
+  device.start();
 }
 
 
 void loop() {
-  
-  ctrl.frameInit();
-  
-  switch(ctrl.getGameMode()){
 
-  case 0:{
-    menu.run();
-    break;
-  }
+  switch(device.getMode()){
+  
+    //
+    case 0:{
+      menu.run();
+      break;
+    }
 
-  case 1:{
-    serpent.run();
-    break;
-    
+    case 1:{
+      menu.run();
+      break;
+    }
+
   }
-  case 2:{
-    repeatMe.run();
-    break;
-    
-  }
-  case 3:
-  case 4:{
-    ctrl.clear();
-    ctrl.print("Entered Game ");
-    ctrl.print(ctrl.getGameMode());
-    ctrl.display();
-  };
-  
-  
-}
 
 
   
